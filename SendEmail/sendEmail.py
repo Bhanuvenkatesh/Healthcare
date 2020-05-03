90c6acdab20820179ea6f2d49ccd7928e13ed322
Bhanuvenkatesh <62827133+Bhanuvenkatesh@users.noreply.github.com>
2020-05-03 23:19:06 +0530

    Update sendEmail.py

--- SendEmail/sendEmail.py
+++ SendEmail/sendEmail.py
@@ -2,7 +2,7 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
-from config_reader import ConfigReader
+#from config_reader import ConfigReader
 
 class EmailSender:
 
